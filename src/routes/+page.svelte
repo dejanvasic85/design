ed5245c8d3133916afe89e2d24b8a9df056ffe86
Dejan Vasic <dejanvasic24@gmail.com>
2024-05-15 16:32:43 +1000

    Adding kitchen sink and block quote styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,29 @@
 <h1>Welcome to our design system!</h1>
 
 <p>
-	Create your package using @sveltejs/package and preview/showcase your work with SvelteKit. Lorem
-	ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-	dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-	aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-	culpa qui officia deserunt mollit anim id est laborum.
+	Create your package using <strong>@sveltejs/package and preview/showcase</strong> your work with
+	<a href="https://kit.svelte.dev">SvelteKit</a>. Lorem ipsum dolor sit amet, consectetur adipiscing
+	elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+	dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+	sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+	laborum.
 </p>
 
 <h2>We use Svelte</h2>
 
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation Lorem ipsum dolor
-	sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-	Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-	Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-	Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-	laborum.
-</p>
+<blockquote>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	<strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit</strong>, sed do eiusmod tempor
+	incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+	ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+	voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</blockquote>
 
 <h3>Lorem ipsum</h3>
+<h4>Lorem ipsum</h4>
+<h5>Lorem ipsum</h5>
 
 <p>
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
@@ -29,9 +32,39 @@
 	esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
 	in culpa qui officia deserunt mollit anim id est laborum.
 </p>
+<hr />
 
 <ul>
 	<li>The hunger games</li>
 	<li>Harry potter</li>
 	<li>Lord of the rings</li>
 </ul>
+
+<hr />
+
+<form>
+	<div>
+		<label for="name">Name:</label>
+		<input type="text" id="name" name="name" />
+	</div>
+
+	<div>
+		<label for="email">Email:</label>
+		<input type="email" id="email" name="email" />
+	</div>
+
+	<div>
+		<label for="number">Message:</label>
+		<textarea id="number" name="number"></textarea>
+	</div>
+
+	<div>
+		<label for="sex">Sex:</label>
+		<select id="sex">
+			<option value="Male">Male</option>
+			<option value="Female">Female</option>
+		</select>
+	</div>
+
+	<button type="submit">Submit</button>
+</form>
