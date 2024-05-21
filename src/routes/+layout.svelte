2fd527523565ceaf8be6f7ccc77fd763bd96a8db
Dejan Vasic <dejanvasic24@gmail.com>
2024-05-21 22:26:47 +1000

    Header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,15 @@
 	import '../style.css';
 </script>
 
+<header>
+	<h1 class="fake-logo">Fake logo</h1>
+	<nav>
+		<a href="#">Home</a>
+		<a href="#">About</a>
+		<a href="#">Contact</a>
+	</nav>
+</header>
+
 <main class="layout">
 	<slot />
 </main>
-
-<footer>Footer coming soon</footer>
